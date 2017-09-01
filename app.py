bacc6f003784ac383e55591b8e24c724414b3151
RyosukeTakahashi <ramuniku@gmail.com>
2017-09-02 00:14:12 +0900

    a level that can be in public

--- app.py
+++ app.py
@@ -1,48 +1,46 @@
 # -*- coding: utf-8 -*-
-
-#  Licensed under the Apache License, Version 2.0 (the "License"); you may
-#  not use this file except in compliance with the License. You may obtain
-#  a copy of the License at
-#
-#       https://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#  License for the specific language governing permissions and limitations
-#  under the License.
-
 from __future__ import unicode_literals
 
 import os
 import sys
+from dotenv import load_dotenv
 import pprint
 import re
-from argparse import ArgumentParser
-from dotenv import load_dotenv
 import requests
 import urllib.parse as urlparse
+from argparse import ArgumentParser
 from flask import Flask, request, abort
-from linebot import (
-    LineBotApi, WebhookParser
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
+from linebot import (LineBotApi, WebhookParser)
+from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, LocationMessage,
     PostbackEvent, JoinEvent, FollowEvent, TemplateSendMessage, CarouselTemplate, CarouselColumn,
     ButtonsTemplate, PostbackTemplateAction, MessageTemplateAction, URITemplateAction
-
 )
 
-# from apiclient.discovery import build
+CHATBOT_ENDPOINT = 'https://chatbot-api.userlocal.jp/api/chat'
+SIMPLE_WIKIPEDIA_API = 'http://wikipedia.simpleapi.net/api'
+PLACES_TEXTSEARCH_ENDPOINT = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
+PLACES_NEARBYSEARCH_ENDPOINT = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+PLACES_DETAIL_ENDPOINT = 'https://maps.googleapis.com/maps/api/place/details/json'
+PLACES_PHOTO_ENDPOINT = 'https://maps.googleapis.com/maps/api/place/photo'
+GEOCODING_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json'
 
 # get CHANNEL_SECRET and CHANNEL_ACCESS_TOKEN from your environment variable
 ENV = load_dotenv('.env')
 CHANNEL_SECRET = os.environ.get('CHANNEL_SECRET')
 CHANNEL_ACCESS_TOKEN = os.environ.get('CHANNEL_ACCESS_TOKEN')
 PLACES_APIKEY = os.environ.get('PLACES_APIKEY')
+GEOCODING_APIKEY = os.environ.get('GEOCODING_APIKEY')
+
+AREA_COUNT = {
+  '天久保': 4,
+  '桜': 3,
+  '春日': 4,
+  '吾妻': 4,
+  '竹園': 4,
+}
+
 
 if CHANNEL_SECRET is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
@@ -54,12 +52,7 @@ if CHANNEL_ACCESS_TOKEN is None:
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(CHANNEL_SECRET)
 
-CHATBOT_ENDPOINT = 'https://chatbot-api.userlocal.jp/api/chat'
-SIMPLE_WIKIPEDIA_API = 'http://wikipedia.simpleapi.net/api'
-PLACES_API_ENDPOINT = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
-
 app = Flask(__name__)
-
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -78,139 +71,241 @@ def callback():
 
     for event in events:
 
-        print("")
+        print("showing event")
         pprint.pprint(event)
         print("")
-
-        # add event bool if needed
 
         if isinstance(event, MessageEvent):
 
             if isinstance(event.message, TextMessage):
 
-                if event.message.text == "hello":
-                    line_bot_api.reply_message(
-                        event.reply_token,
-                        TextSendMessage(text="Hi")
-                    )
-                if event.message.text == "go":
-                    line_bot_api.reply_message(
-                        event.reply_token,
-                        get_budget_buttons_template_message()
-                    )
-                if event.message.text == "restaurant":
-                    places = get_places_by_textsearch('1', 'onfoot')["results"]
-                    pprint.pprint(places[:6])
-                    messages = [TextSendMessage(text=place['name']) for place in places[:4]]
+                text = event.message.text
 
+                if text in AREA_COUNT.keys():
                     line_bot_api.reply_message(
                         event.reply_token,
-                        messages
+                        get_area_buttons_template_message(text)
                     )
+
+            if isinstance(event.message, LocationMessage):
+
+                latitude = event.message.latitude
+                longtitude = event.message.longitude
+
+                # line_bot_api.reply_message(
+                #     event.reply_token,
+                #     get_budget_buttons_template_message()
+                # )
 
         if isinstance(event, PostbackEvent):
 
-            postback_data = event.postback.data
+            data = event.postback.data
+            postback_data_dict = get_postback_data_dict(data)
+            try:
+                next = postback_data_dict['next']
+            except:
+                next = ''
 
-            if "choose_budget" in postback_data:
-
-                budget = event.postback.data[-1] #  last letter is budget level
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    [TextSendMessage(text="budget level is {}.".format(budget)),
-                     TextSendMessage(text="移動手段は?"),
-                     get_transportation_buttons_template_message(budget)]
-                )
-
-            if "choose_transportation" in postback_data:
-
-                postback_data = event.postback.data
-                options = dict(urlparse.parse_qsl(postback_data))
-                places = get_places_by_textsearch(options['budget'], options['transportation'])["results"]
-                pprint.pprint(places[6])
-                # リスト内包表記をしたあとだと、異なるクラスのインスタンスは、アペンドできない？
+            if 'budget' in next:
 
                 line_bot_api.reply_message(
                     event.reply_token,
-                    get_spot_carousel(places[:5])
+                    get_budget_buttons_template_message(data)
                 )
 
+            elif "transportation" in next:
 
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    get_transportation_buttons_template_message(data)
+                )
 
+            elif "showresult" in next:
+
+                location_geometry = get_geocode(postback_data_dict['area'])
+
+                places = get_places_by_textsearch(
+                    postback_data_dict['budget'],
+                    postback_data_dict['transportation'],
+                    location_geometry
+                )["results"]
+
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    get_spot_carousels(places[:5])
+                )
+
+            if "detail" in data:
+
+                place_id = dict(urlparse.parse_qsl(data))['id'] # id in postback_data. Not the result json.
+                place_detail = get_place_detail(place_id)['result']
+
+                if 'map' in data:
+                    messages = [TextSendMessage(text=place_detail['url'])]
+                if 'phone' in data:
+                    messages = [TextSendMessage(text=place_detail['formatted_phone_number'])]
+
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    messages
+                )
 
     return 'OK'
 
 
-def get_budget_buttons_template_message():
+#######################################
+
+# Below are templates functions
+
+def get_area_buttons_template_message(area):
+
+    chou_count = AREA_COUNT[area]
+
+    actions = [PostbackTemplateAction(
+                    label='{}丁目'.format(str(i)),
+                    text='{}の{}丁目'.format(area, str(i)), # it would send a text after postback
+                    data='area={}{}&next=budget'.format(area, str(i))
+                ) for i in range(1, chou_count+1)]
+
+    buttons_template_message = TemplateSendMessage(
+        alt_text='{}の何丁目にいますか？'.format(area),
+        template=ButtonsTemplate(
+            title='{}の何丁目にいますか？'.format(area),
+            text='お選びください',
+            actions=actions
+        )
+    )
+
+    return buttons_template_message
+
+
+def get_budget_buttons_template_message(postback_data):
+
+    postback_data_without_next = re.sub('&next=.*', '', postback_data)
+
+    actions = [PostbackTemplateAction(
+                    label=budget_range,
+                    text="{}なところですね。".format(budget_range),
+                    data=postback_data_without_next + '&budget=' + str(i+1) + '&next=transportation'
+                )for i, budget_range in enumerate(['安め', '普通', '高め'])]
 
     buttons_template_message = TemplateSendMessage(
         alt_text='予算を決めるボタンが表示されています',
         template=ButtonsTemplate(
-            thumbnail_image_url='https://example.com/image.jpg',
             title='予算はどの程度ですか？',
             text='お選びください',
-            actions=[
-                PostbackTemplateAction(
-                    label='安いbudget level 1',
-                    # text='postback text', # it would send a text after postback
-                    data='action=choose_budget&budget=1'
-                ),
-                PostbackTemplateAction(
-                    label='普通budget level 2',
-                    data='action=choose_budget&budget=2'
-                ),
-                PostbackTemplateAction(
-                    label='高めbudget level 3',
-                    data='action=choose_budget&budget=3'
-                ),
-                # MessageTemplateAction(
-                #     label='message',
-                #     text='message text'
-                # ),
-                # URITemplateAction(
-                #     label='uri',
-                #     uri='http://example.com/'
-                # )
-            ]
+            actions=actions
         )
     )
 
     return buttons_template_message
 
 
-def get_transportation_buttons_template_message(budget):
+def get_transportation_buttons_template_message(postback_data):
+
+    postback_data_without_next = re.sub('&next=.*', '', postback_data)
+
+    actions = [PostbackTemplateAction(
+                    label=transportation,
+                    text='{}で移動します。'.format(transportation),
+                    data=postback_data_without_next + '&transportation=' + transportation + '&next=showresult'
+                )for transportation in ['徒歩', '自転車', '車']]
 
     buttons_template_message = TemplateSendMessage(
         alt_text='移動手段を決めるボタンが表示されています',
         template=ButtonsTemplate(
-            thumbnail_image_url='https://example.com/image.jpg',
             title='移動手段は？',
             text='お選びください',
-            actions=[
-                PostbackTemplateAction(
-                    label='徒歩',
-                    text='budget{}、で徒歩で移動ですね。'.format(budget),  # it would send a text after postback
-                    data='action=choose_transportation&transportation=onfoot&budget=' + budget
-                ),
-                PostbackTemplateAction(
-                    label='自転車',
-                    text='budget{}、で自転車で移動ですね。'.format(budget),
-                    data='action=choose_transportation&transportation=bicycle&budget=' + budget
-                ),
-                PostbackTemplateAction(
-                    label='車',
-                    text='budget{}、で車で移動ですね。'.format(budget),
-                    data='action=choose_transportation&transportation=car&budget=' + budget
-                ),
-            ]
+            actions=actions
         )
     )
 
     return buttons_template_message
 
 
-def get_places_by_textsearch(budget, transportation):
+def get_spot_carousels(places5):
+
+    columns = [get_carousel_column_template(place) for place in places5]
+    # todo template.py のCarouselTemplate(Base)をCarouselTemplate(Template)に変えたほうがいいような
+    carousel_template_message = TemplateSendMessage(
+        alt_text='候補地が表示されています。',
+        template=CarouselTemplate(columns=columns)
+    )
+
+    return carousel_template_message
+
+
+def get_carousel_column_template(place):
+
+    area = re.sub('日本、[\s\S]?(〒\d{3}-\d{4}[\s\S]?)?茨城県つくば市', '', place['formatted_address'])
+
+    gmap_url = get_place_detail(place['place_id'])['result']['url']
+
+    try:
+        photo_url = get_place_photo_url(place['photos'][0]['photo_reference'])
+    except:
+        photo_url = 'https://developers.google.com/maps/documentation/static-maps/images/quota.png'
+
+    # 住所が長すぎると予算を表示するスペースが無くなる問題。
+    line_change = '\n'
+    if len(area) > 17:
+        line_change = ''
+
+    if place['price_level'] is 1:
+        price = '~1200円'
+    if place['price_level'] is 2:
+        price = '1200円~'
+    if place['price_level'] is 3:
+        price = '3000円~'
+
+    address = '住所: {}\nレビュー: {} {}予算: {}'.format(
+        area, str(place['rating']),
+        line_change, price
+    )
+
+    carousel_column = CarouselColumn(
+                    thumbnail_image_url=photo_url,
+                    title=place['name'],
+                    text=address,
+                    actions=[
+                        URITemplateAction(
+                            label='地図とレビューを見る',
+                            uri=gmap_url
+                        ),
+                        PostbackTemplateAction(
+                            label='電話をする',
+                            data='action=detail_phone&id={}'.format(place['place_id'])
+                        )
+                    ]
+    )
+
+    return carousel_column
+
+# get template function end
+
+###############################################
+
+# Below are api using function
+
+
+def get_geocode(address):
+
+    params = {
+        'address': 'つくば市 ' + address,
+        'key': GEOCODING_APIKEY
+    }
     s = requests.Session()
+    r = s.get(GEOCODING_ENDPOINT, params=params)
+    json = r.json()
+    pprint.pprint(json)
+    location = json['results'][0]['geometry']['location']
+
+    location_str = str(location['lat']) + ',' + str(location['lng'])
+
+    return location_str
+
+
+def get_places_by_textsearch(budget, transportation, location_geometry):
 
     radius = ''
     if transportation is 'onfoot':
@@ -219,14 +314,13 @@ def get_places_by_textsearch(budget, transportation):
         radius = '2000'
     elif transportation is 'car':
         radius = '8000'
-    print(radius)
 
     params = {
         'key': PLACES_APIKEY,
-        'location': "36.110277, 140.100987",
+        'location': location_geometry,
         'radius': radius,
         'maxprice': budget,
-        'minprice': '1',
+        'minprice': str(int(budget)-1),
         # aquarium cafe art_gallery bar bowling_alley museum movie_theater meal_delivery meal_takeaway
         # zoo spa restaurant
         'type': 'restaurant',
@@ -234,56 +328,53 @@ def get_places_by_textsearch(budget, transportation):
         'rankby': 'prominence',
         'language': 'ja'
     }
-
-    r = s.get(PLACES_API_ENDPOINT, params=params)
+    s = requests.Session()
+    r = s.get(PLACES_TEXTSEARCH_ENDPOINT, params=params)
     json_result = r.json()
 
     return json_result
 
 
-def get_spot_carousel(places5):
+def get_place_detail(place_id):
 
-    columns = [get_carousel_column_template(place) for place in places5]
-    # template.py のCarouselTemplate(Base)をCarouselTemplate(Template)に変えないと
-    # おそらくできない。Requestがおかしいとエラー表示される. 要検証。少なくともTemplateに変えてもできる。
-    carousel_template_message = TemplateSendMessage(
-        alt_text='Carousel template',
-        template=CarouselTemplate(
-            columns=columns
-        )
-    )
+    params = {
+        'key': PLACES_APIKEY,
+        'placeid': place_id,
+        'language': 'ja'
+    }
 
-    return carousel_template_message
+    s = requests.Session()
+    r = s.get(PLACES_DETAIL_ENDPOINT, params=params)
+    json_result = r.json()
 
+    # pprint.pprint(json_result)  # encode error
 
-def get_carousel_column_template(place):
-
-    area = re.sub('日本、[\s\S]?〒\d{3}-\d{4}[\s\S]?茨城県つくば市', '', place['formatted_address'])
-    carousel_column = CarouselColumn(
-                    thumbnail_image_url=place['icon'],
-                    title=place['name'],
-                    text='area:{}\nreview:{}\nbudget:{}'.format(area, str(place['rating']), str(place['price_level'])),
-                    actions=[
-                        PostbackTemplateAction(
-                            label="地図を見る",
-                            data='action=map&id={}'.format(place['id'])
-                        ),
-                        PostbackTemplateAction(
-                            label="電話をする",
-                            data='action=phone&id={}'.format(place['id'])
-                        ),
-                        PostbackTemplateAction(
-                            label="レビューを見る",
-                            data='action=review&id={}'.format(place['id'])
-                        ),
-                    ]
-    )
-
-    return carousel_column
+    return json_result
 
 
+def get_place_photo_url(photo_ref):
 
+    params = {
+        'key': PLACES_APIKEY,
+        'photoreference': photo_ref,
+        'maxwidth': '400'
+    }
+    url = PLACES_PHOTO_ENDPOINT + '?' + urlparse.urlencode(params)
+
+    return url
+
+# api using function end.
+
+#####################################
+
+
+def get_postback_data_dict(data):
+    return dict(urlparse.parse_qsl(data))
+
+
+#####################################
 if __name__ == "__main__":
+
     arg_parser = ArgumentParser(
         usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
     )
