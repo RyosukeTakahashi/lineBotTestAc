1c00ce9c57d4f8bf17167cbd8a4f10b4bea7e9c1
RyosukeTakahashi <ramuniku@gmail.com>
2017-09-09 10:47:14 +0900

    testing CI 2nd.

--- test_app.py
+++ test_app.py
@@ -1,5 +1,7 @@
 import unittest
 import app
+import json
+import pprint
 
 
 class TestLineBot(unittest.TestCase):
@@ -11,8 +13,27 @@ class TestLineBot(unittest.TestCase):
         location_geometry = "36.1096719,140.1113418"
 
         output = app.get_places_by_nearby_search(budget, transportation, location_geometry)
-
         expected = []
 
         self.assertIsNot(output, expected)
 
+    def test_get_index(self):
+
+        with open('place.json', encoding='utf-8') as f:
+            places = json.load(f)["results"]
+
+        result_count = len(places)
+
+        nth_result = 2 # data_dict['nth-result']
+
+        start_index = int(nth_result) * 5
+        end_index = 5 + int(nth_result) * 5
+
+        if end_index > result_count:
+            end_index = result_count
+            start_index = end_index - (end_index % 5)
+
+        pprint.pprint(places[start_index:end_index])
+
+        self.assertEqual(start_index, 0)
+        self.assertEqual(end_index, 3)
