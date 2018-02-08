cdf7f99d2e7239c7f4ee7d4f06fd4ecca7e40252
RyosukeTakahashi <ramuniku@gmail.com>
2018-02-08 16:11:47 +0900

    change runtime to 3.6.2

--- app.py
+++ app.py
@@ -397,8 +397,16 @@ def get_carousel_column_template(place):
         price = 0
         address_template = '住所: {}\nレビュー: {}'
 
+
+
+    if 'rating' not in place.keys():
+        rating = 'なし'
+    else:
+        rating = str(place['rating'])
+
+
     address = address_template.format(
-        area, str(place['rating']),
+        area, rating,
         line_change, price
     )
 
